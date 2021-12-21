4b31afc3e70b68541814a934330a0431c7e9777a
Demetrius Vignati <demetriusvas@gmail.com>
2021-12-21 19:38:36 -0300

    Atualização de todos os arquivos

--- js/validacao.js
+++ js/validacao.js
@@ -1,11 +1,11 @@
-export function valida(input){
+export function valida(input) {
     const tipoDeInput = input.dataset.tipo
 
-    if(validadores[tipoDeInput]){
+    if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
 
-    if(input.validity.valid){
+    if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
@@ -23,11 +23,11 @@ const tiposDeErro = [
 
 const mensagensDeErro = {
     nome: {
-        valueMissing: 'O campo nome não pode estar vazio.'
+        valueMissing: 'O campo de nome não pode estar vazio.'
     },
     email: {
-        valueMissing: 'O campo de e-mail não pode estar vazio.',
-        typeMismatch: 'O email digitado não é valido'
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
     },
     senha: {
         valueMissing: 'O campo de senha não pode estar vazio.',
@@ -35,75 +35,75 @@ const mensagensDeErro = {
     },
     dataNascimento: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
-        customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
-        customError: 'O CPF digitado não é valido.'
+        customError: 'O CPF digitado não é válido.' 
     },
     cep: {
         valueMissing: 'O campo de CEP não pode estar vazio.',
         patternMismatch: 'O CEP digitado não é válido.',
         customError: 'Não foi possível buscar o CEP.'
     },
-    logradouro: { 
+    logradouro: {
         valueMissing: 'O campo de logradouro não pode estar vazio.'
     },
-    cidade: { 
+    cidade: {
         valueMissing: 'O campo de cidade não pode estar vazio.'
     },
-    estado: { 
+    estado: {
         valueMissing: 'O campo de estado não pode estar vazio.'
     }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
-    cpf: input => validaCPF(input),
-    cep: input => recuperarCEP(input)
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
-function mostraMensagemDeErro(tipoDeInput, input){
+function mostraMensagemDeErro(tipoDeInput, input) {
     let mensagem = ''
     tiposDeErro.forEach(erro => {
-        if(input.validity[erro]){
-            mensagem = mensagensDeErro[tipoDeInput[erro]]
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
         }
     })
+    
     return mensagem
 }
 
-function validaDataNascimento (input){
+function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
     let mensagem = ''
 
-    if(!maiorQue18(dataRecebida)){
-        mensagem = 'Voce deve ser maior que 18 anos para se cadastrar.'
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
-
 
     input.setCustomValidity(mensagem)
 }
 
-function maiorQue18(data){
+function maiorQue18(data) {
     const dataAtual = new Date()
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
 }
 
-function validaCPF(input){
+function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '')
     let mensagem = ''
 
-    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
-        mensagem = 'O CPF digitado não é valido.'
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
     }
 
     input.setCustomValidity(mensagem)
 }
 
-function checaCPFRepetido(cpf){
+function checaCPFRepetido(cpf) {
     const valoresRepetidos = [
         '00000000000',
         '11111111111',
@@ -114,13 +114,12 @@ function checaCPFRepetido(cpf){
         '66666666666',
         '77777777777',
         '88888888888',
-        '99999999999',
+        '99999999999'
     ]
-
     let cpfValido = true
 
     valoresRepetidos.forEach(valor => {
-        if (valor == cpf){
+        if(valor == cpf) {
             cpfValido = false
         }
     })
@@ -128,15 +127,14 @@ function checaCPFRepetido(cpf){
     return cpfValido
 }
 
-function checaEstruturaCPF(cpf){
+function checaEstruturaCPF(cpf) {
     const multiplicador = 10
 
     return checaDigitoVerificador(cpf, multiplicador)
 }
 
-function checaDigitoVerificador(cpf, multiplicador){
-
-    if(multiplicador >= 12){
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12) {
         return true
     }
 
@@ -144,23 +142,23 @@ function checaDigitoVerificador(cpf, multiplicador){
     let soma = 0
     const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
     const digitoVerificador = cpf.charAt(multiplicador - 1)
-    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
         soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
         contador++
     }
 
-    if(digitoVerificador == confirmaDigito(soma)){
+    if(digitoVerificador == confirmaDigito(soma)) {
         return checaDigitoVerificador(cpf, multiplicador + 1)
     }
 
     return false
 }
 
-function confirmaDigito(soma){
+function confirmaDigito(soma) {
     return 11 - (soma % 11)
 }
 
-function recuperarCEP(input){
+function recuperarCEP(input) {
     const cep = input.value.replace(/\D/g, '')
     const url = `https://viacep.com.br/ws/${cep}/json/`
     const options = {
@@ -172,27 +170,26 @@ function recuperarCEP(input){
     }
 
     if(!input.validity.patternMismatch && !input.validity.valueMissing) {
-        fetch(url, options).then(
+        fetch(url,options).then(
             response => response.json()
         ).then(
             data => {
-                if(data.erro){
-                    input.setCustomValidity('Não foi possível buscar o CEP')
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
                     return
                 }
                 input.setCustomValidity('')
-                preencheCamposComCep(data)
+                preencheCamposComCEP(data)
                 return
             }
         )
     }
-
 }
 
-function preencheCamposComCep(data){
-    const logradouro = document.querySelector('[data-tipo="logradouro"')
-    const cidade = document.querySelector('[data-tipo="cidade"')
-    const estado = document.querySelector('[data-tipo="estado"')
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
 
     logradouro.value = data.logradouro
     cidade.value = data.localidade
